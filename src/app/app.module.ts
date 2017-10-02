9e159cd86311c69380ec36cc6a6d8e0d2b297a97
Frosthage <mikael.frosthage@consid.se>
2017-10-02 13:01:46 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+import { NgModule, NO_ERRORS_SCHEMA, LOCALE_ID } from '@angular/core'
+import { BrowserModule, Title } from '@angular/platform-browser'
+
+import { FormsModule } from '@angular/forms';
+
+import { KomponentkartanApplicationComponent } from './app.component';
+import { KomponentkartaComponent } from './komponentkarta/komponentkarta.component'
+import { FormatmallComponent } from './formatmall/formatmall.component'
+import { FargkartaComponent } from './fargkarta/fargkarta.component'
+import { MenuSelectorComponent } from './menuSelector/menuSelector.component'
+
+import { KomponentkartanModule } from '../component-package/komponentkartan.module';
+import { ListsComponent } from './lists/lists.component';
+import { CalendarsComponent } from './calendars/calendars.component';
+import { InputFieldsComponent } from './inputFields/inputFields.component'
+import { CityService } from './inputFields/cityService';
+
+import { RouterModule, Routes } from '@angular/router'
+import { appRoutes } from './routes'
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    KomponentkartanModule,
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    KomponentkartanApplicationComponent,
+    KomponentkartaComponent,
+    FormatmallComponent,
+    FargkartaComponent,
+    MenuSelectorComponent,
+    ListsComponent,
+    CalendarsComponent,
+    InputFieldsComponent
+  ],
+  providers: [
+    CityService, { provide: LOCALE_ID, useValue: 'sv-SE' }
+  ],
+
+  bootstrap: [KomponentkartanApplicationComponent]
 })
 export class AppModule { }
+
+
